ca10eb315a3e54e3e8426e7171b619dd6daf519c
oumacode <136460127+oumacode@users.noreply.github.com>
2025-08-01 16:41:42 +0100

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <view
+    style={
+      {
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"#376E8C"
+      }
+    }>
+      
+    </view>
+  );
+}
